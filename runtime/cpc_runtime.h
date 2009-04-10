c5e8a366d65f277d45fe1e2273abe4a6ef213f78
Gabriel Kerneis <kerneis@pps.jussieu.fr>
2009-04-10 10:01:55 +0200

    Remove #pragmas in cpc_runtime.h

--- runtime/cpc_runtime.h
+++ runtime/cpc_runtime.h
@@ -5,7 +5,6 @@ Experimental; do not redistribute.
 #include <stdio.h>
 #include <string.h>
 
-#pragma cilnoremove("type cpc_continuation", "type cpc_condvar")
 struct cpc_continuation;
 typedef void cpc_function(void*);
 typedef struct cpc_condvar cpc_condvar;
@@ -27,7 +26,6 @@ struct cpc_continuation *cpc_continuation_copy(struct cpc_continuation *c);
 
 extern cpc_continuation *cpc_ready_1;
         
-#pragma cilnoremove("cpc_alloc")
 static inline void* 
 cpc_alloc(struct cpc_continuation **cp, int s)
 {
@@ -43,7 +41,6 @@ cpc_alloc(struct cpc_continuation **cp, int s)
     return p;
 }
 
-#pragma cilnoremove("cpc_dealloc")
 static inline void*
 cpc_dealloc(struct cpc_continuation *c, int s)
 {
@@ -51,7 +48,6 @@ cpc_dealloc(struct cpc_continuation *c, int s)
     return c->c + c->length;
 }
 
-#pragma cilnoremove("cpc_really_invoke_continuation")
 static void
 cpc_really_invoke_continuation(struct cpc_continuation *c)
 {
@@ -76,7 +72,6 @@ cpc_really_invoke_continuation(struct cpc_continuation *c)
 #ifdef CPC_TAIL_RECURSIVE_COMPILER
 #define cpc_invoke_continuation cpc_really_invoke_continuation
 #else
-#pragma cilnoremove("cpc_invoke_continuation")
 static inline void
 cpc_invoke_continuation(struct cpc_continuation *c)
 {
@@ -88,7 +83,6 @@ cpc_invoke_continuation(struct cpc_continuation *c)
 #define CPC_IO_IN 1
 #define CPC_IO_OUT 2
 
-#pragma cilnoremove("cpc_continuation_push")
 static inline struct cpc_continuation *
 cpc_continuation_push(cpc_continuation *c, cpc_function *f)
 {
@@ -100,7 +94,6 @@ cpc_continuation_push(cpc_continuation *c, cpc_function *f)
     return c;
 }
 
-#pragma cilnoremove("cpc_continuation_patch")
 static inline void
 cpc_continuation_patch(cpc_continuation *cont, size_t size, void *value)
 {
@@ -111,21 +104,16 @@ cpc_continuation_patch(cpc_continuation *cont, size_t size, void *value)
   return;
 }
 
-#pragma cilnoremove("cpc_schedule")
 void cpc_schedule(struct cpc_continuation *cont);
 extern void cpc_main_loop(void);
-#pragma cilnoremove("cpc_prim_sleep")
 extern void cpc_prim_sleep(int, int, cpc_condvar*, cpc_continuation*);
 cpc_condvar *cpc_condvar_get(void);
 cpc_condvar *cpc_condvar_retain(cpc_condvar*);
 void cpc_condvar_release(cpc_condvar*);
-#pragma cilnoremove("cpc_prim_wait")
 void cpc_prim_wait(cpc_condvar*, cpc_continuation*);
 void cpc_signal(cpc_condvar*);
 void cpc_signal_all(cpc_condvar*);
-#pragma cilnoremove("cpc_prim_io_wait")
 void cpc_prim_io_wait(int, int, cpc_condvar*, cpc_continuation*);
 
-#pragma cilnoremove("cpc_prim_attach", "cpc_prim_detach")
 void cpc_prim_attach(cpc_continuation*);
 void cpc_prim_detach(cpc_continuation*);
