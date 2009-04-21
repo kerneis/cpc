096d327b042173562792f678c31fbd0ddd7bfe2a
Gabriel Kerneis <kerneis@pps.jussieu.fr>
2009-04-21 19:21:57 +0200

    Use bin/cpc.asm.exe everywhere

--- build.sh
+++ build.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
-set -o nounset
 set -e
 
-sh configure
-make clean
-ocamlbuild -Xs runtime,samples cil.otarget
-cp _build/src/main.native obj/x86_LINUX/cilly.asm.exe
+# Adventurous people daring to try ocamlbuild might use the following
+# lines. It worked pretty well for me but was a PITA to maintain in
+# parallel with CIL build system.
+# Warning: this is not compatible with make, so if you try to switch
+# from one to the another, ocamlbuild will ask you to run a sanitize
+# script. You might also have to remove doc/html.
+
+#ocamlbuild -Xs runtime,samples cil.otarget
+#cp _build/src/main.native bin/cpc.asm.exe
+#ln -sfn ../_build/doc/cil.docdir doc/html
+
+# Most people should use this (not so insane) default instead
+make
+
+# The following does not depend on the choice made above
 cd runtime
-make clean
-make
+make clean all
 cd ../samples
-make clean
-make
+make clean all
 cd ..
