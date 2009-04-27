90241e08d18d5aa8eeca2674c6fbf5f81239981b
Gabriel Kerneis <kerneis@pps.jussieu.fr>
2009-04-27 15:10:48 +0200

    Fixes for ocamlbuild support

--- build.sh
+++ build.sh
@@ -2,21 +2,10 @@
 
 set -e
 
-# Adventurous people daring to try ocamlbuild might use the following
-# lines. It worked pretty well for me but was a PITA to maintain in
-# parallel with CIL build system.
-# Warning: this is not compatible with make, so if you try to switch
-# from one to the another, ocamlbuild will ask you to run a sanitize
-# script. You might also have to remove doc/html.
-
-#ocamlbuild -Xs runtime,samples cil.otarget
-#cp _build/src/main.native bin/cpc.asm.exe
-#ln -sfn ../_build/doc/cil.docdir doc/html
-
-# Most people should use this (not so insane) default instead
+# If you want to use ocamlbuild:
+# make BUILDER=ocamlbuild
+# Otherwise:
 make
-
-# The following does not depend on the choice made above
 cd runtime
 make clean all
 cd ../samples
--- clean.sh
+++ clean.sh
@@ -2,12 +2,7 @@
 
 set -e
 
-# For people using ocamlbuild (cf. build.sh)
-# rm -rf _build bin/cpc.asm.exe doc/html
-
-# For other people
 make distclean
-
 cd runtime
 make clean
 cd ../samples
