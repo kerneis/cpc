5f950fe0952d98c2c15ccb134ba391dac6f3b8fd
Gabriel Kerneis <kerneis@pps.jussieu.fr>
2009-05-11 11:17:14 +0200

    Exhaustive pattern-matching in llvmgen.ml

--- src/ext/llvmgen.ml
+++ src/ext/llvmgen.ml
@@ -822,6 +822,8 @@ class llvmGeneratorClass : llvmGenerator = object (self)
       | Block b -> gBlock slabel b
       | TryFinally (_, _, _) -> raise (Unimplemented "TryFinally")
       | TryExcept (_, _, _, _) -> raise (Unimplemented "TryExcept")
+      | CpcFun _ | CpcIoWait _ | CpcSleep _ | CpcWait _ | CpcSpawn _
+      | CpcCut _ -> raise (Unimplemented "Cpc construct")
 
     and gReturnVoid (label:string) sterm sbrk scont : llvmBlock = 
       mkBlock label [] (TRet [])
